465dbe9cb30fcf56d14b4b057f4727c7d9c6303f
Johnny S <46267620+johnnystrada@users.noreply.github.com>
2022-09-22 11:15:57 -0500

    Add files via upload
    Python file used to extract U.S. based song information only from a larger worldwide Spotify dataset.

---
+++ spotifydata.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 20:13:00 2022
+
+@author: John S. Strada
+Description: This script is meant to open a large csv file, read it, 
+& apply a condition based and then write
+the partioned data to a new smaller csv file.
+"""
+import pandas as pd #imports the panda module
+data = pd.read_csv("C:\\Data\\spotify_songs.csv", encoding="utf8") # opens the original csv for reading and creates the data frame
+#data_filtered = data[(data.region == 'United States') & (data.chart == 'top200')] # creates a filtered data frame based on conditions
+#data_filtered.to_csv("C:\\Data\\songsupdate.csv", index=False) # writes filtered data frame to new csv file
+data.to_csv("C:\\Data\\spotify_songsupdate.csv", encoding="utf8", index=False)
+# data.groupby('year').sample(50000) additional code that can be used to sample the dataset

ef969f0977c2e814130701e957554d87828b8765
Johnny S <46267620+johnnystrada@users.noreply.github.com>
2023-06-19 11:48:32 -0500

    workflow template

---
+++ data_analysis_wftemp.py
@@ -0,0 +1,8 @@
+# This is a general template for a data analysis workflow
+
+### IMPORT PACKAGES ###
+import pandas as pd
+import numpy as np
+
+### DATA SET EVALUATION ###
+df = pd.read_csv(dataset.csv)
